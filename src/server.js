e1e1298b33b2535f6d986ed4e349eb4534a4a6b3
ChenLiangliang <chenliangliang2016@gmail.com>
2016-12-05 00:43:17 +0800

    config

---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strcit';
+/*practice Node.js
+ *@author Liangliang chen
+ */
+import path from 'path';
+import ProjectCore from 'ProjectCore';
+
+const $ = global.$ = new ProjectCore();
+
+//加载配置文件
+$.init.add((done) => {
+    $.config.load(path.resolve(__dirname, 'config.js'));
+    const env = process.env.NODE_ENV || null;
+    if (env) { env = ''
+        $.config.load(path.resolve(__dirname, '../config', env + '.js' ));
+    }
+    $.env = env;
+    done();
+});
+
+//初始化
+$.init((err) => {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    } else {
+        console.log('inited [env = %s]', $.env);
+    }
+});
